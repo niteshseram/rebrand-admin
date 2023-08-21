d6092d032769f2b8418e0f5d5693d42195c34180
Seram Nitesh Singh <nitesh.s@auzmor.com>
2023-08-21 22:05:43 +0530

    Initialize project with nextjs, tailwind, shadcn ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <>
+            <p>Hello Admin Dashboard</p>
+            <div className="p-4">
+            <Button>Click Me</Button>
+            </div>
+        </>
+    )
+}
