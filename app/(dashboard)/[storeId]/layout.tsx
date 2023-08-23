23a0a0a7393161426a41bf94d10e5682f63b2bb3
Nitesh Seram <niteshseram@gmail.com>
2023-08-23 21:57:40 +0530

    Add Navbar with store switcher

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,8 @@
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 
+import Navbar from '@/components/navbar'
+
 import prismadb from '@/lib/prismadb'
 
 const DashboardLayout = async ({
@@ -29,7 +31,7 @@ const DashboardLayout = async ({
 
 	return (
 		<>
-			<div>This will be a Navbar</div>
+			<Navbar />
 			{children}
 		</>
 	)
