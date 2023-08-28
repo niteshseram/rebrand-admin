3e2120a7254e1aa7bb4002b26a701d51094ab89b
Nitesh Seram <niteshseram@gmail.com>
2023-08-28 09:14:31 +0530

    Add orders entity

---
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -0,0 +1,48 @@
+import { OrderClient } from './components/client'
+import { OrderColumn } from './components/columns'
+
+import prismadb from '@/lib/prismadb'
+import { priceFormatter } from '@/lib/utils'
+
+const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
+	const orders = await prismadb.order.findMany({
+		where: {
+			storeId: params.storeId,
+		},
+		include: {
+			orderItems: {
+				include: {
+					product: true,
+				},
+			},
+		},
+		orderBy: {
+			createdAt: 'desc',
+		},
+	})
+
+	const formattedOrders: OrderColumn[] = orders.map((item) => ({
+		id: item.id,
+		phone: item.phone,
+		address: item.address,
+		products: item.orderItems
+			.map((orderItem) => orderItem.product.name)
+			.join(', '),
+		totalPrice: priceFormatter.format(
+			item.orderItems.reduce((total, item) => {
+				return total + Number(item.product.price)
+			}, 0)
+		),
+		isPaid: item.isPaid,
+	}))
+
+	return (
+		<div className='flex-col'>
+			<div className='flex-1 space-y-4 p-8 pt-6'>
+				<OrderClient data={formattedOrders} />
+			</div>
+		</div>
+	)
+}
+
+export default OrdersPage
